c37a100c6e04b89c20e3185f21e57a6435fea952
Zack Stout <zackstout@gmail.com>
2018-03-06 21:39:51 -0600

    interesting, seems to be running 2.5 times faster here than in NetBeans

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,7 +8,23 @@ public class HelloWorld {
     public static void main(String[] args) {
         // TODO code application logic here
         // Ok must be double quotes:
-        System.out.println("Hello you world!" + args[0]);
+        // System.out.println("Hello you world!" + args[0]);
+
+        int[] nums = new int[500000];
+        long start = System.currentTimeMillis();
+        // Ok must be double quotes:
+        System.out.println("Hello you dark world!");
+        for (int i=0; i < 500; i++) {
+            for (int j=0; j < 1000; j++) {
+                nums[i] = i;
+                System.out.print(i);
+            }
+//            System.out.println(" ");
+        }
+        long end = System.currentTimeMillis();
+
+        System.out.println("   ");
+        System.out.print(end - start);
     }
 
 }
