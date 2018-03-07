474b18a9b76e2d27a33cccafd4acb18e304ab95b
Zack Stout <zackstout@gmail.com>
2018-03-06 21:26:12 -0600

    hello world! can't figure out how to run netbeans apps from terminal but that's ok

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+// Hmm something weird with the NetBeans version, couldn't get it to work in terminal.
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Ok must be double quotes:
+        System.out.println("Hello you world!" + args[0]);
+    }
+
+}
